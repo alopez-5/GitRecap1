3e05e6a70d163ed524e2564d398b4b59f4c4d942
alainLZ <alainlopez2154@gmail.com>
2024-08-28 18:14:21 -0700

    new changes

--- src/com/neotech/lesson2/HelloWorld1.java
+++ src/com/neotech/lesson2/HelloWorld1.java
@@ -5,6 +5,8 @@ public class HelloWorld1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
+		
+		System.out.println("This is a new comment on the branch");
 
 	}
 
